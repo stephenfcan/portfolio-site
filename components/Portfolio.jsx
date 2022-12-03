1f4c69792ca02a5587a57e67ff5377c990a52210
Stephen An <stephenan@Stephens-Air.lan>
2022-12-03 02:57:35 -0500

    fix apos escape 1

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -4,7 +4,7 @@ const Portfolio = () => {
   return (
     <div className='min-h-screen h-full py-32'>
       <div className=''>
-        <h1 className='text-white'>Portfolio<span className='ml-4 text-xs font-extralight'>I'm working on it!</span></h1>
+        <h1 className='text-white'>Portfolio<span className='ml-4 text-xs font-extralight'>I&apos;m working on it!</span></h1>
         
       </div>
     </div>
