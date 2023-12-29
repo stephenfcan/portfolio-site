d9319d6498f96f5b71d6370e2293055b19de75b9
Stephen An <stephenfcan@gmail.com>
2023-12-28 21:12:02 -0800

    abnormal

--- components/About.jsx
+++ components/About.jsx
@@ -5,10 +5,10 @@ const About = () => {
   return (
     <div className='min-h-screen h-full pt-28 md:pt-32 py-40 md:flex items-center'>
       <div className='flex flex-col items-center space-y-5 md:flex-row md:items-start md:space-x-5 md:space-y-0'>
-        <img className='h-60 w-60 object-contain lg:h-96 lg:w-96' src="https://user-images.githubusercontent.com/84104582/206887035-f5bff4bc-4335-414e-a9cc-f5d488a4c6fc.JPEG" alt="" />
+        <img className='h-60 w-60 object-contain lg:h-96 lg:w-96' src="https://github.com/stephenfcan/dummy-app/assets/84104582/7abafbd1-f95d-4c1e-bcb9-7bb17c3f97bb" alt="" />
         <div className=''>
           <p className='text-white text-md font-light mb-3'>A Georgia native now residing in Silicon Valley, I consider myself a life-long learner and risk-taker - a constant cycle of failing fast and personal growth. I thrive in uncertainty, exploring new ideas, designing a cohesive vision, and executing relentlessly.</p>
-          <p className='text-white text-md font-light mb-3'>Today, I&apos;m currently at the intersection of product management, full-stack development, and user experience. I&apos;m passionate about building products that have a positive impact on society and even more so when I get to build them alongside incredible talent. Feel free to reach out if you&apos;d like to learn more about my experience, create something amazing together, or just want to <Link href='mailto:stephenfcan@gmail.com'><span className='underline hover:opacity-60'>have a chat</span></Link>.</p>
+          <p className='text-white text-md font-light mb-3'>Today, I&apos;m currently at the intersection of product, engineering, and user experience. I&apos;m passionate about building solutions that have a positive impact and even more so when I get to build them alongside incredible talent. Feel free to reach out if you&apos;d like to learn more about my experience, create something amazing together, or just want to <Link href='mailto:stephenfcan@gmail.com'><span className='underline hover:opacity-60'>have a chat</span></Link>.</p>
           <p className='text-white text-md font-light mb-3'>Let&apos;s get to work.</p>
           <img className='h-32' src="https://user-images.githubusercontent.com/84104582/205430009-475c2ff1-2a24-4719-a71e-569e147949a9.png" alt="" />
         </div>
@@ -16,5 +16,6 @@ const About = () => {
     </div>
   )
 }
-
+//https://github.com/stephenfcan/dummy-app/assets/84104582/d1af5933-fe2f-4eca-8571-05a59b28e1a0
+//https://user-images.githubusercontent.com/84104582/206887035-f5bff4bc-4335-414e-a9cc-f5d488a4c6fc.JPEG
 export default About
--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -6,16 +6,52 @@ const Portfolio = () => {
   return (
     <div className='min-h-screen h-full pt-28 md:pt-32'>
       <div className='text-white text-sm space-y-10 pb-20'>
+      <div className='space-y-2'>
+          {/* Abnormal Job */}
+          <ExperienceBanner
+            date={'Jan 2024 - Present'}
+            title={'Technical Program Manager'}
+            company={'Abnormal Security'}
+            link={'https://abnormalsecurity.com/'}
+            city={'San Francisco'}
+          />
+          <p>Working...</p>
+        </div>
+
+        <div className='space-y-2'>
+          {/* Youtube Channel */}
+          <ExperienceBanner
+            date={'Jan 2023 - Present'}
+            title={'Content Creator'}
+            company={'Youtube Channel'}
+            link={'https://www.youtube.com/@stephen-an'}
+            city={'San Francisco'}
+          />
+          <div className='flex space-x-5 overflow-scroll snap-x scrollbar-hide'>
+            <img className='snap-end rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/216759311-8ef5af75-764e-4b42-b8bd-e6905c7dd05c.jpg" alt="" />
+            <img className='snap-end rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/282406561-8575bfcf-16a7-40c9-b5fb-a8472ee85c98.png" alt="" />
+            <img className='snap-end rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/282406597-9143b30a-4b9a-4960-b28e-b8c97a3321a5.png" alt="" />
+            <img className='snap-center rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/216759136-66994d69-13c8-4d5a-aa94-d27657abf872.jpg" alt="" />
+            <img className='snap-end rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/216759267-41a17d30-f2cb-4db2-b9fe-16a3a0c47d02.jpg" alt="" />
+          </div>
+          <p>Creating educational videos catered towards business and product strategy to learn from some of the most influential companies in the world.</p>
+        </div>
+
         <div className='space-y-2'>
           {/* ServiceNow Job */}
           <ExperienceBanner
-            date={'Jan 2023 - Present'}
-            title={'Program Manager, Engineering & Product Ops'}
+            date={'Jan 2023 - Jan 2024'}
+            title={'Associate Program Manager, Engineering & Product Ops'}
             company={'ServiceNow'}
             link={'https://www.servicenow.com/'}
             city={'Santa Clara'}
           />
-          <p>Responsible for conducting, analyzing, and improving product metrics such as application performance time and availability across dCX properties - Now Learning, Impact Hub, and Now Support. Accelerate product feature definition through customer feedback survey analysis. Assist with RiseUp data initiative, building data collection strategy and Adobe Analytics implementation. Managed financial operations including engineering contract procurement and API license renewals.</p>
+          {/* <div className='flex space-x-5 overflow-scroll snap-x scrollbar-hide'>
+            <img className='snap-end rounded-md md:rounded-2xl' src="https://github.com/stephenfcan/dummy-app/assets/84104582/baa58376-068a-490e-a98a-556c397f3e1b" alt="" />
+            <img className='snap-end rounded-md md:rounded-2xl' src="https://github.com/stephenfcan/dummy-app/assets/84104582/b21c1811-a4bd-435b-b14e-4080adb719db" alt="" />
+            <img className='snap-end rounded-md md:rounded-2xl' src="https://github.com/stephenfcan/dummy-app/assets/84104582/6edfc6fc-6c4a-4edb-964f-ff41d83fc7d5" alt="" />
+          </div> */}
+          <p>Responsible for conducting, analyzing, and improving product and engineering metrics across dCX properties &#40;Now Learning, Impact, and Now Support&#41;, improving average application performance and deployment frequency by 26% and 58% and reducing change failure rate and cycle time by 77% and 66%. Led the development and execution of the dCX Product Operations initaitive, building a Unified Support Model, dashboards, and processes to improve incident prioritization, monitoring, and remediation, resulting in a 10x improvement in MTTR and 0 aged incidents. Managed financial operations including API license renewals and the procurement and delivery of 50+ contract workers.</p>
         </div>
 
         <div className='space-y-2'>
@@ -33,30 +69,13 @@ const Portfolio = () => {
             <img className='snap-center rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/205475400-4de842f5-4138-4ecf-82a6-2ad604159da2.png" alt="" />
             <img className='snap-start rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/205474850-2be4a31f-7ab3-47be-ba91-42744e1c8351.png" alt="" />
           </div> */}
-          <p>Supported execution with engineering teams &#40;Learning Experience Platform & Now Platform, Now Learning Training&#41; to successfully build and deploy features such as Core LXP v1.4-1.8, Partner ETA, AI Search, LaaS API, and AI/ML Recommendations.</p>
-        </div>
-
-        <div className='space-y-2'>
-          {/* Youtube Channel */}
-          <ExperienceBanner
-            date={'Jan 2023 - Present'}
-            title={'Content Creator'}
-            company={'Youtube Channel'}
-            link={'https://www.youtube.com/@stephen-an'}
-            city={'San Francisco'}
-          />
-          <div className='flex space-x-5 overflow-scroll snap-x scrollbar-hide'>
-            <img className='snap-end rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/216759311-8ef5af75-764e-4b42-b8bd-e6905c7dd05c.jpg" alt="" />
-            <img className='snap-center rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/216759136-66994d69-13c8-4d5a-aa94-d27657abf872.jpg" alt="" />
-            <img className='snap-end rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/216759267-41a17d30-f2cb-4db2-b9fe-16a3a0c47d02.jpg" alt="" />
-          </div>
-          <p>Creating educational videos catered towards business and product strategy to learn from some of the most influential companies in the world.</p>
+          <p>Project Manager for engineering teams &#40;LXP & Platform, Now Learning Training&#41;, leading SAFe rituals and resolving blockers to deliver features such as Partner Enterprise Training Agreements, AI Search, and ML Recommendations. Strategized and oversaw E2E, UAT, and performance testing efforts, managing dashboards to track and resolve defects.</p>
         </div>
 
         <div className='space-y-2'>
           {/* UNRELENTING */}
           <ExperienceBanner
-            date={'Aug 2021 - Present'}
+            date={'INCUBATING'}
             title={'Founder'}
             company={'UNRELENTING'}
             link={'https://unrelenting.com/'}
@@ -66,7 +85,7 @@ const Portfolio = () => {
             <img className='snap-end rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/205454940-5d30e26c-be36-4963-8ea4-8e2e1af18043.png" alt="" />
             <img className='snap-start rounded-md md:rounded-2xl' src="https://user-images.githubusercontent.com/84104582/205474294-34c8abed-409c-4d61-861d-3879cd1b80bb.png" alt="" />
           </div>
-          <p>Launching new clothing brand in early-2024. Built ground-up e-commerce web application using Next.js and features global cart state and Stripe Checkout API. Currently collaborating with suppliers to navigate supply chains and apparel design/quality.</p>
+          <p>Built ground-up e-commerce web application using Next.js and features global cart state and Stripe Checkout API.</p>
         </div>
 
         <div className='space-y-2'>
